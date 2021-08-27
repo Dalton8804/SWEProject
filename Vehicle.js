288cd2c0d38c18178eb2d335c01b873574fd5f84
Dalton Avery <58116518+Dalton8804@users.noreply.github.com>
2021-08-27 14:24:21 -0500

    Update Vehicle.js

--- Vehicle.js
+++ Vehicle.js
@@ -1,63 +1,24 @@
-function Vehicle(first, last, age, gender, interests) {
-  this.name = {
-     first : first,
-     last : last
-  };
-  this.age = age;
-  this.gender = gender;
-  this.interests = interests;
-  this.bio = function() {
-    alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
-  };
-  this.greeting = function() {
-    alert('Hi! I\'m ' + this.name.first + '.');
-  };
-}
-
-public class Vehicle{
-	private long VIN = 00000000000000000;
-	private String type = "N/A"; 
-	private String make = "N/A";
-	private String model = "N/A";
-	private String country = "N/A";
-	private int year = 0000;
-	private int mileage = 000000;
-	private String[] addedFeatures;
-	private String size = "N/A";
-	private String color = "N/A";
-	private String engine = "N/A";
-	private String mpg = "N/A";
-	private String fuelType = "N/A";
-	private String currentLocation = "N/A";
-	private int monthlyPayment = 999;
-
-	public Vehicle(long VIN, String type, String make, String model,
-		 String country, int year, int mileage, String[] addedFeatures,
-		 String size, String color, String engine, String mpg, String fuelType,
-		 String currentLocation, int monthlyPayment){
-			 this.VIN = VIN;
-			 this.type = type;
-			 this.make = make;
-			 this.model = model;
-			 this.country = country;
-			 this.year = year;
-			 this.mileage = mileage;
-			 this.addedFeatures = addedFeatures;
-			 this.size = size;
-			 this.color = color; 
-			 this.engine = engine;
-			 this.mpg = mpg; 
-			 this.fuelType = fuelType;
-			 this.currentLocation = currentLocation;
-			 this.monthlyPayment = monthlyPayment;
-	}
-
-	public Vehicle(long VIN){
-		this.VIN = VIN;
-	}
-
-	public long getVIN(){
+function Vehicle(VIN, type, make, model,
+		 country, year, mileage, addedFeatures,
+		 size, color, engine, mpg, fuelType,
+		 currentLocation, monthlyPayments) {
+  	this.VIN = VIN;
+	this.type = type;
+	this.make = make;
+	this.model = model;
+	this.country = country;
+	this.year = year;
+	this.mileage = mileage;
+	this.addedFeatures = addedFeatures;
+	this.size = size;
+	this.color = color; 
+	this.engine = engine;
+	this.mpg = mpg; 
+	this.fuelType = fuelType;
+	this.currentLocation = currentLocation;
+	this.monthlyPayment = monthlyPayments;
+	this.getVin = function() {
 		return this.VIN;
-	}
-
+	};
 }
+
